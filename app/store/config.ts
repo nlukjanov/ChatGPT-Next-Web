ec9e18e6991f52d7644cf934b8d9e550dd654ea7
Nikita Lukjanov <16008663+nlukjanov@users.noreply.github.com>
2026-06-05 12:55:45 +0100

    chore: change default models to gpt5.4nano

--- app/store/config.ts
+++ app/store/config.ts
@@ -64,7 +64,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-5.4" as ModelType,
+    model: "gpt-5.4-nano" as ModelType,
     providerName: "OpenAI" as ServiceProvider,
     temperature: 0.5,
     top_p: 1,
